ee7c2d2e9dd5114e777d48992e9b931d5098fa83
Depak Borhara <depakborhara@gmail.com>
2023-07-06 15:43:27 -0400

    updated with remote db

--- db/db.js
+++ db/db.js
@@ -1,9 +1,13 @@
 const { Sequelize } = require("sequelize");
 const { name } = require("../package.json");
+require("dotenv").config();
 // name === ttpbackend2023
 
-const db = new Sequelize(`postgres://localhost:5432/${name}`, {
-  logging: false,
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${name}`,
+  {
+    logging: false,
+  }
+);
 
 module.exports = db;
