4cb3c6ec9948d70a3022c53e2617cd2dee9db165
Depak Borhara <depakborhara@gmail.com>
2023-07-06 17:14:40 -0400

    blah

--- db/db.js
+++ db/db.js
@@ -5,26 +5,22 @@ const dotenv = require("dotenv");
 dotenv.config();
 // name === ttpbackend2023
 
-// const db = new Sequelize(
-//   "postgres://dborhara:FYR065wSqSIJNzNbOTJoyEcRWdZQ7tSh@dpg-cijhg7p8g3nc2g8ki9f0-a.ohio-postgres.render.com/ttpbackend",
-//   {
-//     logging: false,
-//   }
-// );
-
 const db = new Sequelize(
-  "ttpbackend",
-  "dborhara",
-  "FYR065wSqSIJNzNbOTJoyEcRWdZQ7tSh",
+  "postgres://dborhara:FYR065wSqSIJNzNbOTJoyEcRWdZQ7tSh@dpg-cijhg7p8g3nc2g8ki9f0-a.ohio-postgres.render.com/ttpbackend",
   {
-    host:
-      "dpg-cijhg7p8g3nc2g8ki9f0-a.ohio-postgres.render.com" ||
-      "FYR065wSqSIJNzNbOTJoyEcRWdZQ7tSh",
-    dialect: "postgres",
-    dialectOptions: {
-      
-    },
+    logging: false,
   }
 );
+
+// const db = new Sequelize(
+//   "ttpbackend",
+//   "dborhara",
+//   "FYR065wSqSIJNzNbOTJoyEcRWdZQ7tSh",
+//   {
+//     host: "dpg-cijhg7p8g3nc2g8ki9f0-a.ohio-postgres.render.com",
+//     dialect:
+//       "postgres" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+//   }
+// );
 
 module.exports = db;
